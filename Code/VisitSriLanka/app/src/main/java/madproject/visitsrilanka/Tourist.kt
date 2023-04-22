5c2ba0ae32a78214d0a7b48b4cae68e2ebce2da1
IT21177514-Ransika M.R.T <it21177514@my.sliit.lk>
2023-04-22 13:04:42 +0530

    Tousist Registration Process is Successful

--- Code/VisitSriLanka/app/src/main/java/madproject/visitsrilanka/Tourist.kt
+++ Code/VisitSriLanka/app/src/main/java/madproject/visitsrilanka/Tourist.kt
@@ -1,3 +1,3 @@
 package madproject.visitsrilanka
 
-data class Tourist(val touristName:String?=null,val touristEmail:String?=null,val touristContactNumber:String?=null,val touristCountry:String?=null,val touristPassword:String?=null)
+data class Tourist(val touristName:String?=null,val touristEmail:String?=null,val touristContactNumber:String?=null,val touristCountry:String?=null,val touristPassword:String?=null,val touristProfilePicture:String ?=null)
