0725fc88b5a7a50c4c84039dff293d5689acfd57
IT21177514-Ransika M.R.T <it21177514@my.sliit.lk>
2023-04-24 11:18:47 +0530

    VisitSriLanka deleted

--- VisitSriLanka/app/src/main/java/madproject/visitsrilanka/Tourist.kt
+++
@@ -1,3 +0,0 @@
-package madproject.visitsrilanka
-
-data class Tourist(val touristName:String?=null,val touristEmail:String?=null,val touristContactNumber:String?=null,val touristCountry:String?=null,val touristPassword:String?=null,val touristProfilePicture:String ?=null)
