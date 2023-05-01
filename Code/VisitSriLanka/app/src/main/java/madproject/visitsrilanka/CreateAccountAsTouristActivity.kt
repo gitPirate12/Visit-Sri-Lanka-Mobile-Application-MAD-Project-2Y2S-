a86549a95cce6611e0034b44bec6d5370361ab3f
IT21177514-Ransika M.R.T <it21177514@my.sliit.lk>
2023-05-01 12:08:13 +0530

    tourist registration validation completed

--- Code/VisitSriLanka/app/src/main/java/madproject/visitsrilanka/CreateAccountAsTouristActivity.kt
+++ Code/VisitSriLanka/app/src/main/java/madproject/visitsrilanka/CreateAccountAsTouristActivity.kt
@@ -32,53 +32,76 @@ class CreateAccountAsTouristActivity : AppCompatActivity() {
 
         createAccountButton.setOnClickListener {
 
-            //check whether confirmed password and created password are same
-            val touristCreatedPassword=createdPassword.text.toString()
-            val touristConfirmedPassword=confirmedPassword.text.toString()
+            if(validateInputs()) {
 
-            if(touristCreatedPassword == touristConfirmedPassword){
+                //check whether confirmed password and created password are same
+                val touristCreatedPassword = createdPassword.text.toString()
+                val touristConfirmedPassword = confirmedPassword.text.toString()
 
-                //get Values From Input Fields
-                val userTouristName=touristName.text.toString()
-                val userTouristEmail=touristEmail.text.toString()
-                val userTouristContactNumber=touristContactNumber.text.toString()
-                val userTouristCountry=touristCountry.text.toString()
+                if (touristCreatedPassword == touristConfirmedPassword) {
+
+                    //get Values From Input Fields
+                    val userTouristName = touristName.text.toString()
+                    val userTouristEmail = touristEmail.text.toString()
+                    val userTouristContactNumber = touristContactNumber.text.toString()
+                    val userTouristCountry = touristCountry.text.toString()
 
 
-                //formatting 'userTouristEmail' to 'formattedEmail'
-                //this 'formattedEmail' use as primary key
-                val formattedEmail=userTouristEmail.split(".").toTypedArray()
+                    //formatting 'userTouristEmail' to 'formattedEmail'
+                    //this 'formattedEmail' use as primary key
+                    val formattedEmail = userTouristEmail.split(".").toTypedArray()
 
-                //create Tourist object
-                val tourist=Tourist(userTouristName,userTouristEmail,userTouristContactNumber,userTouristCountry,touristConfirmedPassword,touristImage)
+                    //create Tourist object
+                    val tourist = Tourist(
+                        userTouristName,
+                        userTouristEmail,
+                        userTouristContactNumber,
+                        userTouristCountry,
+                        touristConfirmedPassword,
+                        touristImage
+                    )
 
-                //create AppUsers object
-                val appUser=AppUsers(userTouristEmail,touristConfirmedPassword,"Tourist")
+                    //create AppUsers object
+                    val appUser = AppUsers(userTouristEmail, touristConfirmedPassword, "Tourist")
 
-                //pass data to 'Tourist' in 'Visit Sri Lanka'
-                dataBase=FirebaseDatabase.getInstance().getReference("Tourist")
-                dataBase.child(formattedEmail[0]).setValue(tourist).addOnSuccessListener {
-                    //this code block will execute if data successfully submitted
+                    //pass data to 'Tourist' in 'Visit Sri Lanka'
+                    dataBase = FirebaseDatabase.getInstance().getReference("Tourist")
+                    dataBase.child(formattedEmail[0]).setValue(tourist).addOnSuccessListener {
+                        //this code block will execute if data successfully submitted
 
-                    //pass data  to 'AppUsers' in 'Visit Sri Lanka'
-                    dataBase2=FirebaseDatabase.getInstance().getReference("AppUsers")
-                    dataBase2.child(formattedEmail[0]).setValue(appUser).addOnSuccessListener {
-                        Toast.makeText(this,"You Created Tourist Account Successfully",Toast.LENGTH_SHORT).show()
-                        resetInputFieldsAfterSubmission()
+                        //pass data  to 'AppUsers' in 'Visit Sri Lanka'
+                        dataBase2 = FirebaseDatabase.getInstance().getReference("AppUsers")
+                        dataBase2.child(formattedEmail[0]).setValue(appUser).addOnSuccessListener {
+                            Toast.makeText(
+                                this,
+                                "You Created Tourist Account Successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            resetInputFieldsAfterSubmission()
+                        }.addOnFailureListener {
+                            Toast.makeText(
+                                this,
+                                "Tourist Account Creation is Unsuccessful",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
                     }.addOnFailureListener {
-                        Toast.makeText(this,"Tourist Account Creation is Unsuccessful",Toast.LENGTH_SHORT).show()
+                        //this code block will execute if data not successfully submitted
+                        Toast.makeText(
+                            this,
+                            "Tourist Account Creation is Unsuccessful",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
 
-                }.addOnFailureListener {
-                    //this code block will execute if data not successfully submitted
-                    Toast.makeText(this,"Tourist Account Creation is Unsuccessful",Toast.LENGTH_SHORT).show()
-                }
 
+                }//end if
+                else {
+                    Toast.makeText(this, "Please Check Password Again", Toast.LENGTH_SHORT).show()
+                }//else
 
             }//end if
-            else{
-                Toast.makeText(this,"Please Check Password Again",Toast.LENGTH_SHORT).show()
-            }//else
 
         }//end method setOnClickListener
 
@@ -126,4 +149,83 @@ class CreateAccountAsTouristActivity : AppCompatActivity() {
         confirmedPassword.text.clear()
         uploadProfilePictureImageView.setImageResource(R.drawable.baseline_account_box_24)
     }//end method resetInputFieldsAfterSubmission
+
+    private fun validateInputs():Boolean{
+
+        //validate name
+        if(touristName.text.isEmpty()){
+            touristName.error="Name is Required!!!"
+            return false;
+        }//end if
+
+
+        val nameValidator = "^[\\p{L}\\s]+(?:[\\p{Zs}\\-][\\p{L}\\s]+)*\$".toRegex()
+        if(!nameValidator.matches(touristName.text.toString())){
+            touristName.error="Name has invalid characters!!!"
+            return false;
+        }//end if
+
+        //validate email
+        if(touristEmail.text.isEmpty()){
+            touristEmail.error="Email is Required!!!"
+            return false
+        }//end if
+
+        val emailValidator="^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))\$".toRegex()
+        if(!emailValidator.matches(touristEmail.text.toString())){
+            touristEmail.error="Enter valid Email!!!"
+            return false
+        }//end if
+
+        //validate contact number
+        if(touristContactNumber.text.isEmpty()){
+            touristContactNumber.error="Contact Number is Required!!!"
+            return false
+        }//end if
+
+        val phoneNumberPattern = Regex("^0\\d{9,10}$")
+        if(!phoneNumberPattern.matches(touristContactNumber.text.toString())){
+            touristContactNumber.error="Enter Valid Contact Number"
+            return false
+        }//end if
+
+        //validate tourist country
+        if(touristCountry.text.isEmpty()){
+            touristCountry.error="Country is Required!!!"
+            return false
+        }//end if
+
+        val countryValidator = "^[\\p{L}\\s]+(?:[\\p{Zs}\\-][\\p{L}\\s]+)*\$".toRegex()
+        if(!countryValidator.matches(touristCountry.text.toString())){
+            touristCountry.error="Enter valid Country Name"
+            return false
+        }//end if
+
+        //validate password
+        if(createdPassword.text.isEmpty()){
+            createdPassword.error="Password Creation is Required!!!"
+            return false
+        }//end if
+
+        val createdPasswordValidator=Regex("^.{5,}$")
+        if(!createdPasswordValidator.matches(createdPassword.text.toString())){
+            createdPassword.error="Password should has At least 5 characters, At least one uppercase letter, At least one lowercase letter, At least one digit and At least one special character(e.g. !@#\$%^&*)"
+            return false
+        }//end if
+
+        //validate confirmed password
+        if(confirmedPassword.text.isEmpty()){
+            confirmedPassword.error="Confirmed Password is Required!!!"
+            return false;
+        }//end if
+
+        if(!(createdPassword.text.toString()==confirmedPassword.text.toString())){
+            confirmedPassword.error="Recheck Confirmed Password!!!"
+            return false;
+        }//end if
+
+
+
+        return true
+    }//end function validateInputs()
 }
