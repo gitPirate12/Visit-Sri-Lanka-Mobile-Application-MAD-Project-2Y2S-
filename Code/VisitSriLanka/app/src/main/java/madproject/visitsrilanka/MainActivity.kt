58831e083b3daea0c674b6a000189136934461e3
IT21158568 <99710501+it21158568@users.noreply.github.com>
2023-05-07 20:16:18 +0530

    guide fuction

--- Code/VisitSriLanka/app/src/main/java/madproject/visitsrilanka/MainActivity.kt
+++ Code/VisitSriLanka/app/src/main/java/madproject/visitsrilanka/MainActivity.kt
@@ -1,26 +1,56 @@
-package madproject.visitsrilanka
+package com.example.guide
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        title="Guide menu"
 
-        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+        val seebutton=findViewById<Button>(R.id.seebutton)
+        seebutton.setOnClickListener{
+            val intent=Intent(this,
+                GuideDash::class.java)
+            startActivity(intent)
+        }
 
-        //splash screen activity
-        val handler=Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val moveToMainNavigationPageActivity= Intent(this,MainNavigationPageActivity::class.java)
-            startActivity(moveToMainNavigationPageActivity)
-            finish()
-        },5000)
+        val addbutton=findViewById<Button>(R.id.addguide)
+        addbutton.setOnClickListener{
+            val intent=Intent(this,
+            GuideRegister::class.java)
+            startActivity(intent)
+        }
 
-    }//end method onCreate()
+        val findButton=findViewById<Button>(R.id.findbtn)
+            findButton.setOnClickListener{
+            val intent=Intent(this,
+                ReadGuide::class.java)
+            startActivity(intent)
+        }
+
+        val updateguidebutton=findViewById<Button>(R.id.updateguidebutton)
+            updateguidebutton.setOnClickListener{
+            val intent=Intent(this,
+                UpdateGuide::class.java)
+            startActivity(intent)
+        }
+
+        val deleteguidebutton=findViewById<Button>(R.id.deletebtn)
+            deleteguidebutton.setOnClickListener{
+            val intent=Intent(this,
+                DeleteGuide::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+
+
 }
