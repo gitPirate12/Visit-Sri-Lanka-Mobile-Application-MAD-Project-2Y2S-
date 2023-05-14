ae3b9b1a6fadd7bbf35644d91d3aadc666f28c36
gitPirate12 <aneeqshaffy7@gmail.com>
2023-05-14 22:25:57 +0530

    added some more final tweaks

---
+++ Code/VisitSriLanka/app/src/test/java/madproject/visitsrilanka/RegistrationTest.kt
@@ -0,0 +1,33 @@
+package madproject.visitsrilanka
+
+import org.junit.Assert.*
+import org.junit.Test
+
+
+
+class DriverRegistrationTest {
+
+
+    // Unit test for driver name validation
+    @Test
+    fun testDriverNameValidation() {
+        // Valid driver name
+        assertTrue("John Doe".matches(Regex("^[A-Z][a-zA-Z ]*[a-zA-Z]\$")))
+
+        // Invalid driver name - starts with lowercase letter
+        assertFalse("john Doe".matches(Regex("^[A-Z][a-zA-Z ]*[a-zA-Z]\$")))
+
+        // Invalid driver name - contains special characters
+        assertFalse("John Doe!".matches(Regex("^[A-Z][a-zA-Z ]*[a-zA-Z]\$")))
+    }
+
+    // Unit test for password validation
+    @Test
+    fun testPasswordValidation() {
+        // Valid password - at least 6 characters
+        assertTrue("123456".length >= 6)
+
+        // Invalid password - less than 6 characters
+        assertFalse("123".length >= 6)
+    }
+}
